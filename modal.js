0e587ac7c739c97998a9a1aa710454fb59dcabc0
Michał Karanter <mkaranter@gmail.com>
2017-09-09 12:26:37 +0200

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,93 @@
+//show and hide
+
+$(".img-icon").click(()=> {
+    $("#modal").show();
+})
+
+$("#close-modal").click(()=> {
+    $("#modal").hide();
+})
+
+//set content
+
+//dzikie
+$(".dzik-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/dzik-01.png")
+})
+
+$(".jez-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/jez-01.png")
+})
+
+$(".zajac-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/zajac-01.png")
+})
+
+$(".deer-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/deer-01.png")
+})
+
+$(".sowa-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/sowa-01.png")
+})
+
+$(".wiew-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/wiewiorka-01.png")
+})
+
+$(".lis-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/lis-01-01.png")
+})
+
+$(".inne-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/inne-01-01.png")
+})
+
+//domowe
+$(".chomik-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/chomik-01.png")
+})
+
+$(".kot-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/kot-01.png")
+})
+
+$(".papuga-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/papuga-01.png")
+})
+
+$(".pies-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/pies-01.png")
+})
+
+$(".krolik-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/krolik-01.png")
+})
+
+$(".mouse-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/mouse-01.png")
+})
+
+$(".zolw-icon").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/zolw-01-01.png")
+})
+
+$(".inne-icon-dom").click(()=> {
+    $("#modal").show();
+    $('#modal-icon').attr("src", "img/inne-01-01.png")
+})
